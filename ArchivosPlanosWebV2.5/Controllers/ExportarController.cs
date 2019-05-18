4d32cedc31216afcd5b8527b64d69561233f38ac
DesarrolloProsis <38117134+DesarrolloProsis@users.noreply.github.com>
2019-05-18 11:54:55 -0500

    Nueva plaza: Jorobas 18/05/2019

--- ArchivosPlanosWebV2.5/Controllers/ExportarController.cs
+++ ArchivosPlanosWebV2.5/Controllers/ExportarController.cs
@@ -118,6 +118,9 @@ namespace ArchivosPlanosWebV2._5.Controllers
             else if (Plaza.Value == "086") //Cerro Gordo
                 ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.29.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
+            else if (Plaza.Value == "069") //Jorobas
+                ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.21                                           .221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
+
             else if (Plaza.Value == "041") //Salamanca
                 ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.30.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
