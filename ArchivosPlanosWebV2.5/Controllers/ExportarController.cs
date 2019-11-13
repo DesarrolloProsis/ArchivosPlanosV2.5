628c8804b36db3aab3d19e0e0de6455e10a53ba7
Miguel Ramírez <mike__221@hotmail.com>
2019-11-13 12:41:08 -0600

    Correción en No. Plazas

--- ArchivosPlanosWebV2.5/Controllers/ExportarController.cs
+++ ArchivosPlanosWebV2.5/Controllers/ExportarController.cs
@@ -104,31 +104,31 @@ namespace ArchivosPlanosWebV2._5.Controllers
                     Plaza.Value = "008";
                 }
 
-                else if (Plaza.Value == "007") //Emiliano Zapata
+                else if (Plaza.Value == "107") //Emiliano Zapata
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.167.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "004") //La Venta
+                else if (Plaza.Value == "104") //La Venta
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.164.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "009") //Tres Marías
+                else if (Plaza.Value == "109") //Tres Marías
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.169.227)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "006") //Aeropuerto
+                else if (Plaza.Value == "106") //Aeropuerto
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.166.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "005") //Xochitepec
+                else if (Plaza.Value == "105") //Xochitepec
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.165.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "001") //Alpuyeca
+                else if (Plaza.Value == "101") //Alpuyeca
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.161.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "084") //Francisco Velasco
+                else if (Plaza.Value == "184") //Francisco Velasco
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.184.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "002") //Paso Morelos
+                else if (Plaza.Value == "102") //Paso Morelos
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.162.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
-                else if (Plaza.Value == "003") //Palo Blanco
+                else if (Plaza.Value == "103") //Palo Blanco
                     ConexionDB = "User Id=GEADBA;Password=fgeuorjvne;  Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.3.163.221)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=GEAPROD)))";
 
                 else
